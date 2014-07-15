0a89abc33d12f6f15a87c4a168b2f49ce775a142
Lord Syn <celticruins@gmail.com>
2014-07-14 20:28:03 -0400

    small corrections

--- Decrypt-backup.ps1
+++ Decrypt-backup.ps1
@@ -1,5 +1,4 @@
 ﻿<#	
-	.NOTES
 	===========================================================================
 	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2014 v4.1.54
 	 Created on:   	7/14/2014 3:23 PM
@@ -7,13 +6,12 @@
 	 Organization: 	GFG
 	 Filename:     	
 	===========================================================================
-	.DESCRIPTION
-		A description of the file.
 #>
-$$ = 0
-[int]$xMenuChoiceA = 0
+
+[int]$i = 0;[int]$xMenuChoiceA = 0
 $itunespath = "$env:APPDATA\Apple Computer\MobileSync\Backup"
 $Devices = Get-ChildItem $itunespath | where { $_.Attributes -eq 'Directory' }
+
 clear
 Write-Host “`n`tDecrypt iOS Backup`n” -ForegroundColor Magenta
 Write-Host “`t`tPlease select the Device UDID to decrypt `n” -Fore Cyan
@@ -26,17 +24,14 @@ foreach ($Device in $Devices.name)
 
 while ($xMenuChoiceA -lt 1 -or $xMenuChoiceA -gt $i)
 {
-	[Int]$xMenuChoiceA = read-host "Please enter an option 1 to $i..."
-	
+	[Int]$xMenuChoiceA = read-host "Please enter an option 1 to $i..."	
 	if ($xMenuChoiceA -lt 0 -or $xMenuChoiceA -gt $i)
 	{	
 		Write-Host “`tPlease select one of the options available.`n” -Fore Red; start-Sleep -Seconds 1
 	}
 }
 
-$xMenuChoiceA = $xMenuChoiceA - 1
-$Device = $Devices[$xMenuChoiceA]
+$xMenuChoiceA = $xMenuChoiceA - 1; $Device = $Devices[$xMenuChoiceA]
 
 python .\backup_tool.py $itunespath\$Device Decrypted-$Device
-
 python keychain_tool.py -d "Decrypted-$Device/KeychainDomain/keychain-backup.plist" "Decrypted-$Device/Manifest.plist"
